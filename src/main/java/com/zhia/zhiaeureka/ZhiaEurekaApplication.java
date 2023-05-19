828ef4f4781e3a62abe24d2c82542fc673fdfcb5
lizhi <18345339912@163.com>
2023-05-19 17:21:33 +0800

    Initial commit

---
+++ src/main/java/com/zhia/zhiaeureka/ZhiaEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.zhia.zhiaeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiaEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiaEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhia/zhiaeureka/ZhiaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhia.zhiaeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhiaEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
