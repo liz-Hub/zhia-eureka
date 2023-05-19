0a80ceca27e235b037165bb0987f175f12db5182
lizhi <18345339912@163.com>
2023-05-19 17:42:31 +0800

    commit

--- src/main/java/com/zhia/zhiaeureka/ZhiaEurekaApplication.java
+++ src/main/java/com/zhia/zhiaeureka/ZhiaEurekaApplication.java
@@ -2,7 +2,9 @@ package com.zhia.zhiaeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ZhiaEurekaApplication {
 
